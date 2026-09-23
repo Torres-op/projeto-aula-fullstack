6fe13dd64021a44d44ac360d72a97fd9a4e4bf56
Torres-op <victor.torresop@hotmail.com>
2026-09-23 12:58:51 -0300

    feat: registra Acervo no admin com filtros por tipo e categoria

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Livro, Acervo
+
+class AcervoAdmin(admin.ModelAdmin):
+    list_filter = ['tipo', 'categoria']
+    search_fields = ['livro__titulo']
 
 admin.site.register(Livro)
+admin.site.register(Acervo, AcervoAdmin)
